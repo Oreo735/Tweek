4def6e48eeda4b14d8e82959308c17b0dd78d30b
Orwa Shams <orwa73@outlook.com>
2021-11-29 18:19:41 +0200

    Added Mongo atlas connection

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const dbUrl = process.env.DB_URL;
 
-mongoose.connect("mongodb://localhost:27017/tweekDB", {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
