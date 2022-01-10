9fdf7679c9db70a99b89439fa85d7e7f51912c10
Orwa Shams <orwa73@outlook.com>
2022-01-10 22:54:42 +0200

    added algorithm template and constraint model

---
+++ models/timetable.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const Constraints = require('./constraints')
+const Schema = mongoose.Schema;
---
+++ public/javascripts/genetic.js
@@ -0,0 +1,47 @@
+
+function mutationFunction(phenotype) {
+    // make a random change to phenotype
+    return phenotype;
+}
+
+function crossoverFunction(phenotypeA, phenotypeB) {
+    // move, copy, or append some values from a to b and from b to a
+    return [phenotypeA, phenotypeB];
+}
+
+function fitnessFunction(phenotype) {
+    let score = 0;
+    // use your phenotype data to figure out a fitness score
+    return phenotype.num;
+}
+
+let firstPhenotype = {
+    dummyKey: "dummyValue",
+    num: 0,
+};
+let secondPhenotype = {
+    dummyKey: "dummyValue",
+    num: 4,
+};
+
+function doesABeatB(a, b) {
+    if (a.num < b.num) return true;
+    else return false;
+}
+
+let geneticAlgorithmConstructor = require("geneticalgorithm");
+let geneticAlgorithm = geneticAlgorithmConstructor({
+    mutationFunction: mutationFunction,
+    crossoverFunction: crossoverFunction,
+    fitnessFunction: fitnessFunction,
+    doesABeatBFunction: doesABeatB,
+    population: [firstPhenotype, secondPhenotype],
+});
+
+console.log("Starting with:");
+console.log(firstPhenotype);
+for (let i = 0; i < 100; i++) geneticAlgorithm.evolve();
+let best = geneticAlgorithm.best();
+delete best.score;
+console.log("Finished with:");
+console.log(best);
